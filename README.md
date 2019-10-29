28ebb35b5ee2f0c262f0e5bddda9bb19a9510089
Ximo Guanter Gonzálbez <joaquin.guantergonzalbez@telefonica.com>
2019-10-29 20:08:27 +0100

    0.2.0

---
+++ README.md
@@ -0,0 +1,65 @@
+# Suspicious pods
+
+![crates.io](https://img.shields.io/crates/v/suspicious-pods.svg)
+
+Suspicious pods is a very simple tool, which does a very simple task: print a list of pods in your Kubernetes cluster that might not be working correctly, along with a reason on why that pod is considered suspicious.
+
+Example:
+
+```bash
+$ suspicious-pods -- help
+suspicious-pods 0.2
+Prints a list of k8s pods that might not be working correctly
+
+USAGE:
+    suspicious-pods.exe <namespace>
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+ARGS:
+    <namespace>    The namespace you want to scan [default: default]
+    
+$ suspicious-pods
+
+fluentd-aggregator-0
+  Container: fluentd-aggregator         Restarted { count: 6, exit_code: 1, reason: Some("Error") }
+fluentd-aggregator-2
+  Container: fluentd-aggregator         Restarted { count: 6, exit_code: 1, reason: Some("Error") }
+fluentd-aggregator-3
+  Container: fluentd-aggregator         Restarted { count: 1, exit_code: 1, reason: Some("Error") }
+fluentd-dgjm8
+  Container: fluentd                    ContainerWaiting(Some("PodInitializing"))
+jaeger-es-index-cleaner-1572220860-jd7b4
+  Container: jaeger-es-index-cleaner    ContainerWaiting(Some("ImagePullBackOff"))
+jaeger-es-index-cleaner-1572307260-5w5kn
+  Container: jaeger-es-index-cleaner    ContainerWaiting(Some("ImagePullBackOff"))
+jaeger-operator-5545d554cb-mf5zt
+  Container: jaeger-operator            Restarted { count: 3, exit_code: 137, reason: Some("OOMKilled") }
+kube-graffiti-5dc8765dc5-mxc2g
+  Container: kube-graffiti              Restarted { count: 2, exit_code: 1, reason: Some("Error") }
+prometheus-0
+  Container: prometheus                 Restarted { count: 1, exit_code: 1, reason: Some("Error") }
+prometheus-1
+  Container: prometheus                 Restarted { count: 1, exit_code: 1, reason: Some("Error") }
+thanos-store-gateway-0
+  Container: thanos-store-gateway       Restarted { count: 1, exit_code: 137, reason: Some("OOMKilled") }
+```
+
+This is useful in big deployments, when you have a large number of pods and you just want to get a quick glimpse of what might be failing in your cluster.
+
+## Installation
+
+### Option 1: Precompiled binaries
+
+Head to the releases and download your binary. There are binaries for Windows and Linux. On Windows, you need to have OpenSSL installed on your machine through [vcpkg](https://github.com/Microsoft/vcpkg)
+
+### Option 2: Cargo
+
+Install [rustup](https://rustup.rs/) and run `cargo install suspicious-pods`. If you are on Windows, you need to have OpenSSL installed on your machine through [vcpkg](https://github.com/Microsoft/vcpkg) and set the environment variable `VCPKGRS_DYNAMIC=1`.
+
+
+## Feedback
+
+Feedback and contributions are welcome! Please open an issue or a PR.
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,58 @@
 use clap::{Arg, App};
+use failure::Error;
 use kube::{
   api::{Api, Object},
   client::APIClient,
-  config,
-  Error
+  config
 };
 use k8s_openapi::api::core::v1::{ContainerStatus, PodSpec, PodStatus};
-use std::result::Result;
 
 #[derive(Debug)]
-enum SuspiciousContainerReason<'a> {
-  ContainerWaiting(Option<&'a str>),
-  Restarted { count: i32, exit_code: i32, reason: &'a Option<String> },
+enum SuspiciousContainerReason {
+  ContainerWaiting(Option<String>),
+  Restarted { count: i32, exit_code: i32, reason: Option<String> },
   TerminatedWithError(i32)
 }
 
 #[derive(Debug)]
-struct SuspiciousContainer<'a> {
-  name: &'a str,
-  reason: SuspiciousContainerReason<'a>
+struct SuspiciousContainer {
+  name: String,
+  reason: SuspiciousContainerReason
 }
 
 #[derive(Debug)]
-struct SuspiciousPod<'a> {
-  name: &'a str,
-  suspicious_containers: Vec<SuspiciousContainer<'a>>
+struct SuspiciousPod {
+  name: String,
+  suspicious_containers: Vec<SuspiciousContainer>
 }
 
-fn is_suspicious(p: &Object<PodSpec, PodStatus>) -> Option<SuspiciousPod> {
-  let pod_name = &p.metadata.name;
-  let status = p.status.as_ref()
+type Result<T> = std::result::Result<T, Error>;
+
+fn is_suspicious(p: Object<PodSpec, PodStatus>) -> Option<SuspiciousPod> {
+  let pod_name = p.metadata.name;
+  let status = p.status
     .expect(format!("Cannot get status for pod {}", pod_name).as_str());
-  let statuses: &Vec<ContainerStatus> = status.container_statuses.as_ref()
+  let statuses: Vec<ContainerStatus> = status.container_statuses
     .expect(format!("Cannot get container statuses for pod {}", pod_name).as_str());;
-  let suspicious_containers: Vec<_> = statuses.iter().filter_map(|status: &ContainerStatus| {
-    let container_name = &status.name;
-    let state = status.state.as_ref()
+  let suspicious_containers: Vec<_> = statuses.into_iter().filter_map(|status: ContainerStatus| {
+    let container_name = status.name;
+    let state = status.state
       .expect(format!("Cannot get state for container {} in pod {}", container_name, pod_name).as_str());
     let reason = if status.restart_count > 0 {
-      let last_state = status.last_state.as_ref()
+      let last_state = status.last_state
         .expect(format!("Cannot get last state for container {} in pod {}", container_name, pod_name).as_str())
         .terminated
-        .as_ref()
         .unwrap();
       Some(SuspiciousContainerReason::Restarted {
         count: status.restart_count,
         exit_code: last_state.exit_code,
-        reason: &last_state.reason
+        reason: last_state.reason
       })
-    } else if let Some(waiting_state) = &state.waiting {
-      let msg: Option<&str> = waiting_state.reason.as_ref().or(waiting_state.message.as_ref()).map(String::as_str);
+    } else if let Some(waiting_state) = state.waiting {
+      let msg: Option<String> = waiting_state.reason.or(waiting_state.message);
       Some(SuspiciousContainerReason::ContainerWaiting(msg))
     } else if state.terminated.is_some() && state.terminated.as_ref().unwrap().exit_code != 0 {
-      Some(SuspiciousContainerReason::TerminatedWithError(state.terminated.as_ref().unwrap().exit_code))
+      Some(SuspiciousContainerReason::TerminatedWithError(state.terminated.unwrap().exit_code))
     } else {
       None
     };
@@ -72,9 +72,18 @@ fn is_suspicious(p: &Object<PodSpec, PodStatus>) -> Option<SuspiciousPod> {
   }
 }
 
-fn main() -> Result<(), Error> {
+fn get_suspicious_pods(namespace: &str) -> Result<Vec<SuspiciousPod>> {
+  let config = config::load_kube_config()?;
+  let client = APIClient::new(config);
+  let pods = Api::v1Pod(client).within(namespace).list(&Default::default())?;
+  Ok(pods.items.into_iter()
+    .filter_map(is_suspicious)
+    .collect())
+}
+
+fn main() -> Result<()> {
   let matches = App::new("suspicious-pods")
-    .version("0.1")
+    .version("0.2")
     .about("Prints a list of k8s pods that might not be working correctly")
     .arg(Arg::with_name("namespace")
       .required(true)
@@ -83,16 +92,11 @@ fn main() -> Result<(), Error> {
       .takes_value(true))
     .get_matches();
   let namespace = matches.value_of("namespace").unwrap();
-  let config = config::load_kube_config().expect("failed to load kubeconfig");
-  let client = APIClient::new(config);
-  let pods = Api::v1Pod(client).within(namespace).list(&Default::default())?;
-  let suspicious_pods: Vec<_> = pods.items.iter()
-    .filter_map(is_suspicious)
-    .collect();
+  let suspicious_pods = get_suspicious_pods(namespace)?;
   for pod in suspicious_pods {
-    println!("{:?}", pod.name);
+    println!("{}", pod.name);
     for container in pod.suspicious_containers {
-      println!("  {:?}", container);
+      println!("  Container: {: <25}\t{:?}", container.name, container.reason);
     }
   }
   Ok(())
